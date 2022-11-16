92389c558ea832d0373de5d29c140a1818d0f5e8
laurilyysi <lyysilauri@gmail.com>
2022-11-16 17:32:44 +0200

    added footer

---
+++ hw3/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <p>University of Tartu</p>
+    <p>WAD 2022</p>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background-color: #a3d3c3;
+  font-size: 0.99em;
+}
+</style>
