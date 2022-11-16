866a42b930e13628488dac6d4d555262e5d319ff
laurilyysi <lyysilauri@gmail.com>
2022-11-16 17:46:33 +0200

    footer to main page

--- hw3/src/views/MainPage.vue
+++ hw3/src/views/MainPage.vue
@@ -4,16 +4,21 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <Footer />
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld, Header
+    HelloWorld,
+    Header,
+    Footer
   }
 }
 
