b985ba2cabde535e0b28bc5e032eec44ee700619
laurilyysi <lyysilauri@gmail.com>
2022-11-16 20:01:56 +0200

    reset likes button

--- hw3/src/views/MainPage.vue
+++ hw3/src/views/MainPage.vue
@@ -1,33 +1,30 @@
 <template>
   <Header />
-      <div class="flexBox">
-        <aside class="leftPanel">
-        </aside>
-        <main>
-          <!-- <img src="../assets/spongebob.jpg"> -->
-            <Post />
-        </main>
-        <aside class="rightPanel">
-        </aside>
-    </div>
+  <div class="flexBox">
+    <aside class="leftPanel"></aside>
+    <main>
+      <Post />
+      <button>Reset likes</button>
+    </main>
+    <aside class="rightPanel"></aside>
+  </div>
   <Footer />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import Header from '@/components/Header.vue'
-import Footer from '@/components/Footer.vue'
-import Post from '@/components/Post.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import Post from "@/components/Post.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
     HelloWorld,
     Header,
     Footer,
-    Post
-  }
-}
-
+    Post,
+  },
+};
 </script>
