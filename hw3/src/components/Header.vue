25262c303b04a88ffb85132a20b7245072064676
VeronikaKukk <veronika.kukk@ut.ee>
2022-11-16 17:31:27 +0200

    header added

---
+++ hw3/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav>
+       <router-link to="/">Home</router-link>
+       <router-link to="/about">Sign Up</router-link>
+        <div class="dropdown">
+            <button onclick="toggle()" class="dropbtn">
+                <a class="userPicture"><img src="https://pbs.twimg.com/profile_images/603318855553810432/CXetbed2_400x400.jpg" width="50" alt="user picture"></a>
+            </button>
+            <div id="myDropdown" class="dropdown-content">
+              <a href="#">SpongeBob SquarePants</a>
+              <a href="#">bob.pants@ut.ee</a>
+              <a class="logout-button" href="login.html">logout</a>
+            </div>
+          </div> 
+    </nav>
+</template>
+
+<style>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- hw3/src/views/MainPage.vue
+++ hw3/src/views/MainPage.vue
@@ -1,4 +1,5 @@
 <template>
+  <Header />
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
@@ -8,11 +9,142 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import Header from '@/components/Header.vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,Header
   }
 }
+
 </script>
+<style>
+* {
+   font-family: 'Segoe UI';
+}
+
+body {
+   margin: 0px;
+   background-color: lightcyan;
+}
+
+main {
+   min-height: 82.3vh;
+   background-color: lightcyan;
+}
+
+.header > ul {
+   list-style: none;
+}
+
+.header {
+    background-color: #a3d3c3;
+    position: sticky;
+    top: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    z-index: 1;
+}
+.nav a {
+   padding: 10px 10px;
+   text-align: center;
+   display: flex;
+   color: #000000;
+   font-size: .99em;
+   text-decoration: none;
+}
+.nav{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    
+}
+
+.nav a:hover {
+   background-color: #f7e948;
+   color: #092747;
+}
+
+.userPicture{
+   padding: 10px 10px;
+}
+
+.border {
+   border: 5px solid #8cd49e;
+   border-radius: 0px;
+}
+
+footer {
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   text-align: center;
+   background-color: #a3d3c3;
+   font-size: .99em;
+}
+
+.dropdown{
+   position: sticky;
+   
+}
+.dropbtn {
+   padding: 10px;
+   font-size: 10px;
+   border: none;
+   cursor: pointer;
+   background-color: #8cd49e;
+}
+
+.dropdown-content {
+   border-radius: 10px;
+   display: none;
+   position: absolute;
+   
+   background-color: #f7e948;
+   overflow: visible;
+   box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+   z-index: 1;
+   right: 1%;
+}
+ 
+.dropdown-content a {
+   border-radius: 10px;
+   color: black;
+   padding: 12px 16px;
+   text-decoration: none;
+   display: block;
+}
+
+.logout-button {
+   padding: 12px 16px;
+}
+
+.logout-button:hover {
+   background-color: #8cd49e;
+   color: #f7e948;
+}
+
+.show {display: block;}
+ 
+.error {
+   text-align: center;
+   font-size: 18px;
+   font-weight: bold;
+   color: maroon;
+   background-color: pink;
+   border: 5px solid maroon;
+   border-radius: 0px;
+   transition: 0.1s;
+}
+
+.error:hover {
+   background-color: lightcoral;
+}
+::selection{
+   color: yellow;
+   background-color: lightcoral;
+}
+</style>
