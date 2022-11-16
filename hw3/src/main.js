326d95ad10396a1d31cb0a77436fa681384fead9
VeronikaKukk <veronika.kukk@ut.ee>
2022-11-16 17:52:38 +0200

    header

--- hw3/src/main.js
+++ hw3/src/main.js
@@ -4,6 +4,3 @@ import router from './router'
 import store from './store'
 
 createApp(App).use(store).use(router).mount('#app')
-function toggle() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
