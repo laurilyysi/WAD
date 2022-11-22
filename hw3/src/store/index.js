b54d113b5ee5f72c7db1dc1a0396f6aaff2ab511
laurilyysi <92779981+laurilyysi@users.noreply.github.com>
2022-11-22 18:45:33 +0200

    fixed audio again

--- hw3/src/store/index.js
+++ hw3/src/store/index.js
@@ -52,7 +52,7 @@ export default createStore({
         time: "Aug 5, 2022",
         textcontent: "Nothing makes me feel better than a laugh.",
         recordingcontent:
-          "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/30158499-38d7-4516-909a-849e09065099/res_laugh.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221121%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221121T111131Z&X-Amz-Expires=86400&X-Amz-Signature=c42ad57f1b925aacb36b687c08ab4bc14b41bd1d658a92417b2fbf0e2b877b8d&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22res_laugh.mp3%22&x-id=GetObject",
+          "https://cdn.sndup.net/m3nm/res_laugh.mp3?token=uw3dpwc-t5BwMt6EL3tMHhV8eV_JBwgxVFtYPLgusd4&token_path=%2Fm3nm%2F&expires=1669136214",
       },
       {
         id: 6,
@@ -72,7 +72,7 @@ export default createStore({
         time: "May 10, 2022",
         textcontent: "Gotta get that secret recipe, I am hungry",
         recordingcontent:
-          "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/43262ce6-45d4-4d84-9fff-e2ff36093b69/res_evilLaugh.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221121%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221121T111504Z&X-Amz-Expires=86400&X-Amz-Signature=82fbdaaf3d404c8676f048ab8911ec0f946af10c67949aab66c681b971a12bc5&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22res_evilLaugh.mp3%22&x-id=GetObject",
+          "https://cdn.sndup.net/xngs/res_evilLaugh.mp3?token=zucJDh36LqB2pEIySlLOxURqDtZ22pj8bT2SQvkQDXk&token_path=%2Fxngs%2F&expires=1669136296",
       },
       {
         id: 8,
