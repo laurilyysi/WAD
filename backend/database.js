030d574a4e1f7581070740eb85bc3b4cc150cc60
VeronikaKukk <veronika.kukk@ut.ee>
2022-12-02 19:49:51 +0200

    date is fixed

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
-  password: "admin",
+  password: "1234",
   database: "testWad",
   host: "localhost",
   port: "5432",
