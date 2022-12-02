ae5de151f355d03d8a4135f9fe41ffdad4f1895c
VeronikaKukk <veronika.kukk@ut.ee>
2022-12-02 14:47:01 +0200

    get posts and insert posts added

--- backend/server.js
+++ backend/server.js
@@ -16,3 +16,32 @@ app.use(express.json());
 app.listen(port, () => {
     console.log("Server is listening to port " + port)
 });
+
+
+
+// get all posts (postList)
+app.get('/api/posts', async(req, res) => {
+    try {
+        console.log("get posts request has arrived");
+        const posts = await pool.query(
+            "SELECT * FROM posttable"
+        );
+        res.json(posts.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// insert post to database
+app.post('/api/posts', async(req, res) => {
+    try {
+        console.log("a post request has arrived");
+        const post = req.body;
+        const newpost = await pool.query( // annab promise
+            "INSERT INTO posttable(\"like\", \"userPicture\", time, textcontent) values ($1, $2, $3, $4)    RETURNING*", [post.like, post.userPicture, post.time, post.textcontent]
+        );
+        res.json(newpost);
+    } catch (err) {
+        console.error(err.message);
+    }
+}); 
