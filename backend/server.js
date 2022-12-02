6ef00f3f50c99de410ce72694780e22ed719c632
VeronikaKukk <veronika.kukk@ut.ee>
2022-12-02 14:55:12 +0200

    delete, view and updated post added

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.post('/api/posts', async(req, res) => {
     try {
         console.log("a post request has arrived");
         const post = req.body;
-        const newpost = await pool.query( // annab promise
+        const newpost = await pool.query(
             "INSERT INTO posttable(\"like\", \"userPicture\", time, textcontent) values ($1, $2, $3, $4)    RETURNING*", [post.like, post.userPicture, post.time, post.textcontent]
         );
         res.json(newpost);
@@ -45,3 +45,48 @@ app.post('/api/posts', async(req, res) => {
         console.error(err.message);
     }
 }); 
+
+// delete post based on id
+app.delete('/api/posts/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        console.log("delete a post request has arrived");
+        const deletepost = await pool.query(
+            "DELETE FROM posttable WHERE id = $1", [id]
+        );
+        res.json(deletepost);
+    } catch (err) {
+        console.error(err.message);
+    }
+}); 
+
+// get only one post based on id
+app.get('/api/posts/:id', async(req, res) => {
+    try {
+        console.log("get a post with route parameter  request has arrived");
+        const { id } = req.params; 
+        const posts = await pool.query(
+            "SELECT * FROM posttable WHERE id = $1", [id]
+        );
+        res.json(posts.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+}); 
+
+
+
+// update only one post based on id
+app.put('/api/posts/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const post = req.body;
+        console.log("update request has arrived");
+        const updatepost = await pool.query(
+            "UPDATE posttable SET (title, body, urllink) = ($2, $3, $4) WHERE id = $1", [id, post.title, post.body, post.urllink]
+        );
+        res.json(updatepost);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
