52531c754d28439e597747387f93e561ca20654e
VeronikaKukk <veronika.kukk@ut.ee>
2022-12-02 13:43:58 +0200

    database connection

--- backend/database.js
+++ backend/database.js
@@ -7,4 +7,33 @@ const pool = new Pool({
     port: "5432"
 });
 
+const execute = async(query) => {
+    try {
+        await pool.connect();
+        await pool.query(query);
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    }
+};
+
+const createTblQuery = `
+    CREATE TABLE IF NOT EXISTS "posttable" (
+	    "id" SERIAL PRIMARY KEY,
+        "like" INTEGER NOT NULL,
+        "userPicture" VARCHAR(500) NOT NULL,
+        "time" VARCHAR(20) NOT NULL,
+        "textcontent" VARCHAR(500) NOT NULL,
+        "imagecontent" VARCHAR(500),
+        "videocontent" VARCHAR(500),
+        "recordingcontent" VARCHAR(500) 
+    );`;
+
+execute(createTblQuery).then(result => {
+    if (result) {
+        console.log('If does not exists, create the "posttable" table');
+    }
+});
+
 module.exports = pool;
