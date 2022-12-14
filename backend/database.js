767ddfba46d9d771835c9f2873dd800207f6afdc
MarekMurumäe <marek.murumae@gmail.com>
2022-12-14 18:37:20 +0200

    adds 7 posts to database if database is empty on startup

--- backend/database.js
+++ backend/database.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
@@ -16,33 +18,76 @@ const execute = async (query) => {
     console.error(error.stack);
     return false;
   }
-};
-const addIfEmptyQuery = `INSERT INTO "posttable" ("like", "userPicture", "time", "textcontent")
-                          SELECT 0, 'https://assets.stickpng.com/thumbs/5874cd0342e4d628738559e1.png', 
+}; 
+const addIfEmptyQuery = `INSERT INTO "posttable" ("userPicture", "time", "textcontent")
+                          SELECT 'https://pbs.twimg.com/profile_images/603318855553810432/CXetbed2_400x400.jpg', 
                           '2022-05-06', 'I am brody, and I will eat vegetables'
-                          WHERE NOT EXISTS (SELECT * FROM "posttable")` 
+                          WHERE NOT EXISTS  (SELECT * FROM "posttable")`;
+const addIf1Query = `INSERT INTO "posttable" ("userPicture", "time", "textcontent")
+                      SELECT 'https://pbs.twimg.com/profile_images/603318855553810432/CXetbed2_400x400.jpg', 
+                      '2022-05-06', 'I am God, and I will eat vegetables'
+                      WHERE (SELECT count("textcontent") FROM "posttable") < 2`; 
+const addIf2Query = `INSERT INTO "posttable" ("userPicture", "time", "textcontent")
+                          SELECT 'https://pbs.twimg.com/profile_images/603318855553810432/CXetbed2_400x400.jpg', 
+                          '2022-05-06', 'I am Tony, and I will not eat vegetables'
+                          WHERE (SELECT count("textcontent") FROM "posttable") < 3`;
+const addIf3Query = `INSERT INTO "posttable" ("userPicture", "time", "textcontent")
+                      SELECT 'https://pbs.twimg.com/profile_images/603318855553810432/CXetbed2_400x400.jpg', 
+                      '2022-05-06', 'I am Mac, and I will eat vegetables'
+                      WHERE (SELECT count("textcontent") FROM "posttable") < 4`;
+const addIf4Query = `INSERT INTO "posttable" ("userPicture", "time", "textcontent")
+                      SELECT 'https://pbs.twimg.com/profile_images/603318855553810432/CXetbed2_400x400.jpg', 
+                      '2022-05-06', 'I am brody2, and I will eat vegetables'
+                      WHERE (SELECT count("textcontent") FROM "posttable") < 5`;
+const addIf5Query = `INSERT INTO "posttable" ("userPicture", "time", "textcontent")
+                  SELECT 'https://pbs.twimg.com/profile_images/603318855553810432/CXetbed2_400x400.jpg', 
+                  '2022-05-06', 'I am God2, and I will eat vegetables'
+                  WHERE (SELECT count("textcontent") FROM "posttable") < 6`;
+const addIf6Query = `INSERT INTO "posttable" ("userPicture", "time", "textcontent")
+                      SELECT 'https://pbs.twimg.com/profile_images/603318855553810432/CXetbed2_400x400.jpg', 
+                      '2022-05-06', 'I am Tony2, and I will eat vegetables'
+                      WHERE (SELECT count("textcontent") FROM "posttable") < 7`;
 
 const createTblQuery = `
     CREATE TABLE IF NOT EXISTS "posttable" (
 	    "id" SERIAL PRIMARY KEY,
-        "like" INTEGER NOT NULL,
         "userPicture" VARCHAR(500) NOT NULL,
         "time" DATE NOT NULL,
         "textcontent" VARCHAR(500) NOT NULL
     );`;
 
 execute(createTblQuery).then((result) => {
-  if (result) {
-    console.log('If does not exists, create the "posttable" table');
-  }
-}).then(
-  execute(addIfEmptyQuery).then((result) => {
-  if(result){
-    console.log('Added post to posttable')
-  }
-}));
-
-
+  if(result) {
+    console.log("if didn't already exist, created posttable");
+    execute(addIfEmptyQuery).then((result) => {
+      if(result){
+        console.log('Added 1st post to posttable');
+        execute(addIf1Query).then((result) => {
+          if(result) {
+            console.log("added 2nd post to posttable");
+            execute(addIf2Query).then((result) => {
+              if(result){
+                console.log('Added 3rd post to posttable');
+                execute(addIf3Query).then((result) => {
+                if(result) {
+                  console.log("added 4th post to posttable")
+                  execute(addIf4Query).then((result) => {
+                  if(result) {
+                    console.log("added 5th post to posttable")
+                    execute(addIf5Query).then((result) => {
+                      if(result) {
+                        console.log("added 6th post to posttable")
+                        execute(addIf6Query).then((result) => {
+                          if(result) {
+                            console.log("added 7th post to posttable")
+                        }})
+                    }})
+                  }})  
+                }})
+              }})
+            }})
+          }})
+}});
 
 const createUserTblQuery = `
     CREATE TABLE IF NOT EXISTS "users" (
